290c66d01d4bb5d5e4b3b5d2971fdaa95d7a3a88
Nikolay Matyukhin <123973210+matyha345@users.noreply.github.com>
2023-06-29 02:09:21 +0300

    variables z-index

--- src/components/ui/play_button/PlayButton.jsx
+++ src/components/ui/play_button/PlayButton.jsx
@@ -1,12 +1,12 @@
-import './PlayButton.scss'
+import styles from './PlayButton.module.scss'
 import { BsFillPlayFill } from "react-icons/bs";
 
 
 const PlayButton = () => {
 	return (
 		<>
-			<div className='box'>
-				<button className='video-play-button' href='#'>
+			<div className={styles.box}>
+				<button className={styles.btn} href='#'>
 					<span>
 						<BsFillPlayFill  />
 					</span>
